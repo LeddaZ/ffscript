c236d9de0db58869e89a572ec076acfac8a1d14f
Leonardo Ledda <35930217+LeddaZ@users.noreply.github.com>
2019-04-17 14:39:43 +0200

    Update ffscript-win.bat

--- ffscript-win.bat
+++ ffscript-win.bat
@@ -1,11 +1,11 @@
 @echo off
 cls
 color 0f
-title ffmpeg script by LeddaZ (Windows)
-echo This script requires ffmpeg. Download it from https://ffmpeg.zeranoe.com/builds/
-echo Run this file from the \bin folder (where ffmpeg.exe is found) or add ffmpeg.exe to Path.
+title ffscript by LeddaZ (Windows)
+echo This script requires ffmpeg. Follow instructions at https://github.com/LeddaZ/ffscript
+echo Run this file from the bin folder (where ffmpeg.exe is found).
 echo ----------
-echo Copy the path of the source file:
+echo Copy the path of the source file (shift+right-click > copy path or drag and drop the file in this window):
 set /p input=
 echo ----------
 echo Enter the output video format (mp4, avi, etc.):
@@ -14,11 +14,12 @@ echo ----------
 echo The converted video will be saved in your Desktop. Press any key to continue.
 pause
 echo ----------
-title ffmpeg script by LeddaZ (Windows) - Video conversion in progress
-set filename=ffmpeg-%date:~4,2%-%date:~7,2%-%date:~10,8%-%time:~0,2%-%time:~3,2%-%time:~6,2%.%format%
+title ffscript by LeddaZ (Windows) - Video conversion in progress
+set filename=ffmpeg_%date%_%time%.%format%
 ffmpeg -i %input% %USERPROFILE%\Desktop\%filename%
-title ffmpeg script by LeddaZ (Windows)
+title ffscript by LeddaZ (Windows)
 cls
+color a
 echo Video conversion finished. You can find the converted video at %USERPROFILE%\Desktop\%filename%.
 pause
 exit
