4be43a26f13ca890474ebb5e61f9d4d18c00af50
Leonardo Ledda <35930217+LeddaZ@users.noreply.github.com>
2019-05-13 19:57:54 +0200

    Update ffscript-win.bat

--- ffscript-win.bat
+++ ffscript-win.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 color 0f
-title ffscript by LeddaZ (Windows)
-echo This script requires ffmpeg. Follow instructions at https://github.com/LeddaZ/ffscript
+title FFscript by LeddaZ
+echo This script requires ffmpeg. Follow the instructions at https://github.com/LeddaZ/ffscript
 echo Run this file from the bin folder (where ffmpeg.exe is found).
 echo ----------
 echo Drag and drop the source file here:
@@ -14,10 +14,10 @@ echo ----------
 echo The converted video will be saved in your Desktop. Press any key to continue.
 pause
 echo ----------
-title ffscript by LeddaZ (Windows) - Video conversion in progress
+title ffscript by LeddaZ - Video conversion in progress
 set filename=ffmpeg_%date%_%time%.%format%
 ffmpeg -i %input% %USERPROFILE%\Desktop\%filename%
-title ffscript by LeddaZ (Windows)
+title ffscript by LeddaZ
 cls
 color a
 echo Video conversion finished. You can find the converted video at %USERPROFILE%\Desktop\%filename%.
